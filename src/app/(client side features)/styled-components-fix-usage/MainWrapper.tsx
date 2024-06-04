6679dc2f170bab86df079692cf7df23609d63023
arthursereda <arthur.sereda@promova.com>
2024-06-04 22:41:27 +0300

    chore: MainWrapper in CSF group

--- src/app/(client side features)/styled-components-fix-usage/MainWrapper.tsx
+++ src/app/(client side features)/styled-components-fix-usage/MainWrapper.tsx
@@ -6,7 +6,7 @@ const MainWrapper = styled.main`
   width: 100%;
   max-width: 800px;
   padding: 16px 24px;
-  margin: 0px auto;
+  margin: 0 auto;
   border: 1px solid hsl(0deg 0% 50% / 0.3);
   border-radius: 2px;
   background: #2c2929;
